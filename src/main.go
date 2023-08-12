a22db32b64690a7b7a0cf7c8ba108579067d51c0
AyumuShigeta <faliplvsg@gmail.com>
2023-08-12 15:20:17 +0900

    envをload

--- src/main.go
+++ src/main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	log "github.com/sirupsen/logrus"
 
 	"github.com/yzmw1213/demo-api/route"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env file")
+	}
 	router := gin.Default()
-
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+		log.Infof("Defaulting to port %s", port)
+	}
 	route.IndexRoute(router)
-	router.Run(":9090")
+	router.Run(":" + port)
 }
