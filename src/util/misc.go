adb39215e50947a4629e6a854cd5cb0c292b8929
AyumuShigeta <faliplvsg@gmail.com>
2023-10-03 00:08:50 +0900

    OutputBasicを追加

---
+++ src/util/misc.go
@@ -0,0 +1,6 @@
+package util
+
+func ToPrt(i interface{}) *interface{} {
+	// https://qiita.com/ikawaha/items/adb24788f818dc9835f9
+	return &i
+}
--- src/util/out_basic.go
+++ src/util/out_basic.go
@@ -1,5 +1,10 @@
 package util
 
+import (
+	"net/http"
+	"reflect"
+)
+
 type OutputBasic struct {
 	Code    int         // コード
 	Result  string      // 結果
@@ -32,4 +37,78 @@ func (o *OutputBasic) GetError() error {
 		return value
 	}
 	return nil
+}
+
+type OutputBasicInterface interface {
+	GetCode() int
+	GetResult() map[string]interface{}
+}
+
+type OutputBasicServiceInterface interface {
+	GetCode() int
+	GetResult() map[string]interface{}
+	GetError() error
+}
+
+type OutputBasicObject struct {
+	Code    int          // コード
+	Result  string       // 結果
+	OutData *interface{} // 結果オブジェクト
+}
+
+func NewOutputBasicObject(row interface{}) *OutputBasicObject {
+	return &OutputBasicObject{
+		OutData: &row,
+	}
+}
+
+func (o *OutputBasicObject) GetResult() map[string]interface{} {
+	value := reflect.ValueOf(*o.OutData)
+	if value.Kind() == reflect.Slice {
+		return map[string]interface{}{
+			"code":   http.StatusOK,
+			"result": "OK",
+			"list":   o.OutData,
+		}
+	} else {
+		return map[string]interface{}{
+			"code":   http.StatusOK,
+			"result": "OK",
+			"row":    o.OutData,
+		}
+	}
+}
+
+func (o *OutputBasicObject) GetCode() int {
+	return o.Code
+}
+
+type OutputBasicListPaging struct {
+	Code       int          // コード
+	Result     string       // 結果
+	List       *interface{} // 結果オブジェクト配列
+	CountTotal int64        // 総数
+	Page       int64        // ページ番号
+	Limit      int64        // 検索数
+}
+
+func NewOutputBasicListPaging(row interface{}) *OutputBasicListPaging {
+	return &OutputBasicListPaging{
+		List: &row,
+	}
+}
+
+func (o *OutputBasicListPaging) GetResult() map[string]interface{} {
+	return map[string]interface{}{
+		"code":        http.StatusOK,
+		"result":      "OK",
+		"list":        o.List,
+		"count_total": o.CountTotal,
+		"page":        o.Page,
+		"limit":       o.Limit,
+	}
+}
+
+func (o *OutputBasicListPaging) GetCode() int {
+	return o.Code
 }
